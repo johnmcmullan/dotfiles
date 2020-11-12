d6575202367ea04e0c30a19e22a0afc49d669f38
John McMullan <John.McMullan@itiviti.com>
2020-11-12 13:34:35 +0000

    cleanup emacs

--- .emacs.d/elpa/dash-functional-20180107.1618/dash-functional-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/john.mcmullan/.emacs.d/elpa/dash-functional-20180107.1618/dash-functional.el  -*- no-byte-compile: t -*-
-(define-package "dash-functional" "20180107.1618" "Collection of useful combinators for Emacs Lisp" '((dash "2.0.0") (emacs "24")) :commit "9631947f2fbeed58b1d07a3ebc1340a3626b2823" :keywords '("lisp" "functions" "combinators"))
