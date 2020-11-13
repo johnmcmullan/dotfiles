87aabb32bd54be84d0d6f0a118f8dd6e6f9ebe31
johnmcmullan <John.McMullan@itiviti.com>
2020-11-13 16:12:11 +0000

    tidy

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,5 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f2c35f8562f6a1e5b3f4c543d5ff8f24100fae1da29aeb1864bbc17758f52b70" default))
  '(package-selected-packages
-   '(ccls company magit-todos magit clang-format hl-todo elpy pyvenv ws-butler)))
+   '(zenburn-theme ccls company magit-todos magit clang-format hl-todo elpy pyvenv ws-butler)))
