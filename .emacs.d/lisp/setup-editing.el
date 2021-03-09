fdecdb851cc20e02e8ffbd9f29b2c0ae7c17ec5b
johnmcmullan <John.McMullan@itiviti.com>
2021-03-09 18:34:00 +0000

    fixed ctrl-a

--- .emacs.d/lisp/setup-editing.el
+++ .emacs.d/lisp/setup-editing.el
@@ -1,4 +1,7 @@
-;; setup-editing
+;; package --- setup-editing
+;;; Commentary: general emacs setup, keys etc
+
+;;; Code:
 
 ;; ------------------------------------------------------------------
 ;; Key mappings
@@ -10,7 +13,8 @@
 
 (global-set-key (kbd "C-x g") 'goto-line)
 (global-set-key (kbd "C-x o") ' ff-find-other-file)
-(global-set-key (kbd "C-a") 'beginning-of-line-or-indentation)
 (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
 
+(eval-after-load "cc-mode"
+  '(define-key c-mode-base-map (kbd "C-a") 'beginning-of-line-or-indentation))
 (provide 'setup-editing)
