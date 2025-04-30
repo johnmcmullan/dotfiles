f5301d4f256908daf71ab6abea7904e94131ed64
johnmcmullan <John.McMullan@itiviti.com>
2025-04-30 08:55:37 +0000

    emacs 30 scrolling - seems a bit messed though

--- .emacs.d/lisp/setup-editing.el
+++ .emacs.d/lisp/setup-editing.el
@@ -10,6 +10,11 @@
 (global-set-key (kbd "C-x o") 'ff-find-other-file)
 (global-set-key (kbd "C-x f") 'lsp-execute-code-action)
 (global-set-key (kbd "M-3") #'(lambda () (interactive) (insert "#")))
+(global-set-key (kbd "<wheel-up>") 'scroll-down)
+(global-set-key (kbd "<wheel-down>") 'scroll-up)
+(setq scroll-step 2)
+
+
 
 (eval-after-load "cc-mode"
   '(define-key c-mode-base-map (kbd "C-a") 'beginning-of-line-or-indentation))
